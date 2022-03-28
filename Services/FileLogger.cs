d1ea4929664c41feff8313d2f15e8aac4df6cb3c
Balazs Laczko <laczbali@gmail.com>
2022-03-28 20:13:53 +0200

    docs: added todos to file logger

--- Services/FileLogger.cs
+++ Services/FileLogger.cs
@@ -11,6 +11,14 @@ namespace server_api.Services
         /// <param name="message"></param>
         public static void Log(string message)
         {
+            // TODO: rework output:
+            // date - Caller Class\Method
+            // message
+            // ----------------------------
+
+            // TODO: new logs should be on top
+            // TODO: actually add logging to the app
+
             string output = $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} - {message}\n";
             string path = Path.Combine(Directory.GetCurrentDirectory(), "log.txt");
             using (StreamWriter writer = new StreamWriter(path, true))
